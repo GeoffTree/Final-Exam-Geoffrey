e8074f924c81c1afc11faab832f6084a75b04651
Geoff Trenholm <trenholg@sheridancollege.ca>
2025-04-17 12:30:33 -0400

    late commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {ProductsComponentComponent} from './products-component/products-component.component';
+import {ShoppingCartComponentComponent} from './shopping-cart-component/shopping-cart-component.component';
+import {
+  CalculationComponentComponent
+} from './shopping-cart-component/calculation-component/calculation-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, ProductsComponentComponent, ShoppingCartComponentComponent, CalculationComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ProductsComponentComponent} from './products-component/products-component.component';
+import {ShoppingCartComponentComponent} from './shopping-cart-component/shopping-cart-component.component';
+import {CalculationComponentComponent} from './shopping-cart-component/calculation-component/calculation-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'products-component', component: ProductsComponentComponent},
+  {path: 'shoppingcart-component', component: ShoppingCartComponentComponent,
+    children: [
+      {path: 'calculation', component: CalculationComponentComponent}
+    ]
+  }
+];
